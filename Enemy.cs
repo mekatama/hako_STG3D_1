e594adf4328655faf1f30ffc0973b8dda6dacfe9
mekatama <tamatte@hotmail.com>
2017-02-08 22:33:11 +0900

    敵の基本挙動
    ナビメッシュを使用してplayerを追尾する

---
+++ Enemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public GameObject targe;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();	//コンポーネントをキャッシュしておく
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//targetに向かうようになる
+		agent.destination = targe.transform.position;
+	}
+}

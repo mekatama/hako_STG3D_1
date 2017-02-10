e24263c4087f5d072e841b5f86bc94086c160367
mekatama <tamatte@hotmail.com>
2017-02-10 22:50:39 +0900

    タイトルのスタートボタン制御用
    ボタンUIで使用

---
+++ title_controller.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class title_controller : MonoBehaviour {
+	//startボタン用の関数
+	public void StartButtonOn(){
+		Application.LoadLevel("stage1");
+	}
+}

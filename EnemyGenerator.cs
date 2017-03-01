c9032c570e85329fdc6b138c29c0df8a39224333
mekatama <tamatte@hotmail.com>
2017-03-01 22:22:53 +0900

    等間隔に敵を出現させる

---
+++ EnemyGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGenerator : MonoBehaviour {
+	public GameObject enemyObject;			//Enemyプレハブ
+	public float timeOut;
+	private float timeElapsed = 2.0f;
+	
+	void Update () {
+		timeElapsed += Time.deltaTime;
+
+        if(timeElapsed >= timeOut) {
+			//弾を生成する位置を指定する
+//			Vector3 vecSpawnPos	= this.position;
+			//弾を生成する
+			Instantiate( enemyObject, transform.position, transform.rotation);
+			timeElapsed = 0.0f;
+ 		}
+	}
+}

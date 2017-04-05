e4ad529dcec11891c8ecfc73bf2fa0b931176d75
mekatama <tamatte@hotmail.com>
2017-04-05 21:11:44 +0900

    スコア表示のため新規追加

---
+++ GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public Text scoreLabel;
+	public GameObject player;
+	public int total_Score = 0;	//表示される合計スコア
+
+	void Start () {
+	}
+	
+	void Update () {
+		scoreLabel.text = total_Score.ToString("000000");
+	}
+}

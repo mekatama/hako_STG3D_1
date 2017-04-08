85e75e846d42922b95475268969ce122f88d6cda
mekatama <tamatte@hotmail.com>
2017-04-08 15:53:17 +0900

    敵のスコアを総合スコアに反映させる
    インスタンス時にGameControllerコンポーネントを取得てきるようにす

--- Enemy.cs
+++ Enemy.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
-	NavMeshAgent agent;		//ナビメッシュ用
-	Transform player;		//位置情報とか入れるっぽい
-	public int enemyHp;		//EnemyのHP
-	public GameObject item;	//Enemyから出現させるアイテム
+	NavMeshAgent agent;			//ナビメッシュ用
+	Transform player;			//位置情報とか入れるっぽい
+	public int enemyHp;			//EnemyのHP
+	public GameObject item;		//Enemyから出現させるアイテム
+	public int enemy_score1;	//enemyの点数
+	GameObject gameController;	//スコア表示を管理しているオブジェクト入れる用
 
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();							//コンポーネントをキャッシュしておく
 		player = GameObject.FindGameObjectWithTag ("Player").transform;	//Playerタグの位置情報ゲット
+		gameController = GameObject.FindWithTag ("GameController");		//GameControllerオブジェクトを探す
 	}
 	
 	void Update () {
 		//targetに向かうようになる
 //		agent.SetDestination(player.position);	//無駄かもしれないけど毎フレーム追いかける
-		agent.destination = player.position;	//どっちでも動く
+		agent.destination = player.position;	//↑どっちでも動く
 	}
 
 	//他のオブジェクトとの当たり判定
@@ -25,6 +29,11 @@ public class Enemy : MonoBehaviour {
 			enemyHp = enemyHp - s.attackPower;			//Bulletスクリプトの攻撃力をHPから引く
 			Debug.Log(enemyHp);
 			if(enemyHp <= 0){
+				//スコア加算
+				//gcって仮の変数にGameControllerのコンポーネントを入れる
+				GameController gc = gameController.GetComponent<GameController>();
+				//GameControllerで管理しているスコア表示用の変数を使って計算
+				gc.total_Score = gc.total_Score + enemy_score1;
 				//このGameObjectを［Hierrchy］ビューから削除する
 				Destroy(gameObject);
 				//四分の一の確率で回復アイテムを落とす

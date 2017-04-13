d7787f0715a34de44bb5e1e040ed7ae947d20820
mekatama <tamatte@hotmail.com>
2017-04-13 21:19:40 +0900

    連射アップアイテム実装

---
+++ Item_RappidUp1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item_RappidUp1 : MonoBehaviour {
+	public float item_rappidup1;	//連射スピード
+	GameObject player;				//オブジェクト入れる用
+
+	void Start () {
+		player = GameObject.FindWithTag ("Player");		//Playerオブジェクトを探す
+	}
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player"){
+			//スピード加算
+			//pcって仮の変数にPlayerのコンポーネントを入れる
+			Player pc = player.GetComponent<Player>();
+			//Playerで管理しているれんし間隔の変数を使って計算
+			pc.timeOut = pc.timeOut - item_rappidup1;
+			//このGameObjectを［Hierrchy］ビューから削除する
+			Destroy(gameObject);
+		}
+	}
+}

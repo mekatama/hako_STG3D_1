54739c4e7a2f91b4388ff54f29821b99913f0617
mekatama <tamatte@hotmail.com>
2017-04-20 08:27:40 +0900

    ショットレベルアップアイテム
    取得するすると一段階レベルアップ

---
+++ Item_PowerUp1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item_PowerUp1 : MonoBehaviour {
+	public int item_powerup1;		//パワーアップ段階
+	GameObject player;				//オブジェクト入れる用
+
+	void Start () {
+		player = GameObject.FindWithTag ("Player");		//Playerオブジェクトを探す
+	}
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player"){
+			//スピード加算
+			//puって仮の変数にPlayerのコンポーネントを入れる
+			Player pu = player.GetComponent<Player>();
+			//Playerで管理しているショットレベルの変数を使って計算
+			pu.shotLevel = pu.shotLevel + item_powerup1;
+			//このGameObjectを［Hierrchy］ビューから削除する
+			Destroy(gameObject);
+		}
+	}
+}

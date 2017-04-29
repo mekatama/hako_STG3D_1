48d566577b5f2388d9205f6c38e9a95f252cc307
mekatama <tamatte@hotmail.com>
2017-04-29 16:00:21 +0900

    ボム数表示に対応

--- GameController.cs
+++ GameController.cs
@@ -4,13 +4,16 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 	public Text scoreLabel;
+	public Text bomLabel;
 	public GameObject player;
 	public int total_Score = 0;	//表示される合計スコア
+	public int bom_Count = 3;	//表示されるボムの数
 
 	void Start () {
 	}
 	
 	void Update () {
 		scoreLabel.text = total_Score.ToString("000000");
+		bomLabel.text = bom_Count.ToString("00");
 	}
 }

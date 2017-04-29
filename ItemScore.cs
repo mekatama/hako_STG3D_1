55ac275312a721e72d124f444800240c6773e8c9
mekatama <tamatte@hotmail.com>
2017-04-29 15:36:07 +0900

    不要コードの削除

--- ItemScore.cs
+++ ItemScore.cs
@@ -4,19 +4,14 @@ using System.Collections;
 
 public class ItemScore : MonoBehaviour{
 	public static int score = 0;		//scire初期化
-//	private Text text;					//コンポーネントを入れる用
 
 	void Start(){
-//		text = GetComponent<Text>();	//textのコンポーネント頂く
+		//初期値
 		GetComponent<Text>().text = score.ToString("000000");
 	}
 
 	void Update(){
-		//ここで計算してはダメ
-//		score = totalScore + time.timeBonus;
-
-//		score = score + 1;
+		//UI表示更新
 		GetComponent<Text>().text = score.ToString("000000");
-//		text.text = score.ToString();			//text更新
 	}
 }

b1f526d258032b973b5d0fc7e6c6b66409997166
mekatama <tamatte@hotmail.com>
2017-05-16 21:02:02 +0900

    連爆するしないのフラグを敵に追加

--- Enemy.cs
+++ Enemy.cs
@@ -10,6 +10,7 @@ public class Enemy : MonoBehaviour {
 	public int enemy_score1;	//enemyの点数
 	GameObject gameController;	//スコア表示を管理しているオブジェクト入れる用
 	public GameObject renbaku;	//Enemy破壊時の爆発
+	public bool bakuhatu;		//連爆の有無
 
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();							//コンポーネントをキャッシュしておく
@@ -37,8 +38,10 @@ public class Enemy : MonoBehaviour {
 				gc.total_Score = gc.total_Score + enemy_score1;
 				//このGameObjectを［Hierrchy］ビューから削除する
 				Destroy(gameObject);
-				//敵破壊時に連爆
-				Instantiate (renbaku, transform.position, transform.rotation);
+				if(bakuhatu == true){
+					//敵破壊時に連爆
+					Instantiate (renbaku, transform.position, transform.rotation);
+				}
 				//四分の一の確率で回復アイテムを落とす
 				if (Random.Range (0, 4) == 0) {
 					Instantiate (item, transform.position, transform.rotation);
@@ -57,8 +60,10 @@ public class Enemy : MonoBehaviour {
 				gc.total_Score = gc.total_Score + enemy_score1;
 				//このGameObjectを［Hierrchy］ビューから削除する
 				Destroy(gameObject);
-				//敵破壊時に連爆
-				Instantiate (renbaku, transform.position, transform.rotation);
+				if(bakuhatu == true){
+					//敵破壊時に連爆
+					Instantiate (renbaku, transform.position, transform.rotation);
+				}
 				//三分の一の確率で回復アイテムを落とす
 				if (Random.Range (0, 3) == 0) {
 					Instantiate (item, transform.position, transform.rotation);

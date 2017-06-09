03e07789c8a1b653483263b37dd50392f259171b
mekatama <tamatte@hotmail.com>
2017-06-09 20:31:27 +0900

    カメラ追従用スクリプト

---
+++ Camera_p.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_p : MonoBehaviour {
+	Vector3 diff;				//起動時のカメラとプレイヤーの位置関係
+	public float followSpeed;	//補間スピード
+	GameObject player;			//オブジェクト入れる用
+
+	void Start () {
+		player = GameObject.FindWithTag ("Player");		//Playerオブジェクトを探す
+		//pcって仮の変数にPlayerのコンポーネントを入れる
+		Player pc = player.GetComponent<Player>();
+		diff = pc.transform.position - transform.position;	//追従距離の計算
+	}
+	
+	void LateUpdate () {
+		//pcって仮の変数にPlayerのコンポーネントを入れる
+		Player pc = player.GetComponent<Player>();
+		//線形補間関数を使用
+		transform.position = Vector3.Lerp(
+			transform.position,
+			pc.transform.position - diff,
+			Time.deltaTime * followSpeed
+		);
+	}
+}
